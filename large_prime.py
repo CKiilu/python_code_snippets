2e7ecd748257ebd01be80a487fb9d004ff06391f
CKiilu <cndeti@gmail.com>
2016-04-15 16:51:20 +0300

    large_prime: Largest number in list

--- large_prime.py
+++ large_prime.py
@@ -13,15 +13,19 @@
 # Takes as parameter an integer and
 # Returns boolean value true if the value is prime or
 # Returns boolean value false if the value is not prime
-import math
+def get_algorithm_result(args):
+	largest = args[0]
+	for x in args:
+		largest = x if x > largest else largest
+	return largest
 
-def get_algorithm_result():
-	pass
 def prime_number(num):
 	return all( num % x for x in range(2, num))
+	
 def main():
 	print prime_number(4)
 	print prime_number(5)
+	print get_algorithm_result([1,2,3,4,12,34,1,2,3,56,23,89,2,3,5,6,7,8])
 
 if __name__ == '__main__':
 	main()
